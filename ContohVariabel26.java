6ea2cb8cb9276069c301d8c2f68bbdf5e4b4b854
RezaAngelinaFebriyanti <145529391+RezaAngelinaFebriyanti@users.noreply.github.com>
2023-10-03 14:01:51 +0700

    Add files via upload

---
+++ ContohVariabel26.java
@@ -0,0 +1,17 @@
+public class ContohVariabel26 {
+    public static void main (String [] args){
+
+        String salahSatuHobySayaAdalah = "Bermain petak umpet";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.75, tinggi = 1.58;
+
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+}
