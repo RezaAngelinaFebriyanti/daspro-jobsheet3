46baf7f795212bc93fadb5095a20e1fde1333c4a
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2026-03-22 13:41:00 +0700

    Percobaan 1: Penggunaan Variabel

--- ContohVariabel26.java
+++ ContohVariabel26.java
@@ -12,6 +12,5 @@ public class ContohVariabel26 {
         System.out.println("Jenis kelamin: " + jenisKelamin);
         System.out.println("Umurku saat ini: " + _umurSayaSekarang);
         System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
-
     }
 }
